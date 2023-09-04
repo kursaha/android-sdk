dbfaa4751d48176c82629980caabdf340f5732e5
Nishant Pathak <nishant@kursaha.com>
2023-09-04 11:59:41 +0530

    Adding Kursaha class

---
+++ android-sdk/src/main/java/com/kursaha/Kursaha.java
@@ -0,0 +1,9 @@
+package com.kursaha;
+
+import android.app.Application;
+
+public class Kursaha {
+    public static void initialize(Application applicationContext, String apiKey) {
+
+    }
+}
--- app/src/main/java/com/kursaha/MyApplication.java
+++ app/src/main/java/com/kursaha/MyApplication.java
@@ -8,6 +8,8 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        // Initialize the Kursaha SDK
+        Kursaha.initialize(this, "YOUR_API_KEY");
         InteractionLogger.setListener(new InteractionLogger.InteractionListener() {
             @Override
             public void onInteraction(String interactionType, String interactionDetails) {
